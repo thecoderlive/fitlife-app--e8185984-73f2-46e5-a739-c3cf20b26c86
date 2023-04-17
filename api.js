e28700fca2da23c1f1571b8faf54908c3d874b8e
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-04-17 13:14:53 +0530

    Initial commit for file: api.js

---
+++ api.js
@@ -0,0 +1,3 @@
+export const api = {
+  "login": "https://script.google.com/macros/s/AKfycbyezGmmVC7433ZOW6K8rVUiMrLVcbcAdDcX_EaiBU0hbkLO0m43p5Q4SbSV4cOeT64S/exec?method=list&endpoint=login&screen_name=login&ss_url=https%3A//docs.google.com/spreadsheets/d/1d8q48YPJx0QBJ8Yivo_GnKE4z4lhm5uadXOpIfTYp1U/edit&item_id="
+}
